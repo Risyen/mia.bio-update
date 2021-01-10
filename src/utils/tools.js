47a89eec2eceeb78417f4612b75292d02961db0d
Mias <812137533@qq.com>
2021-01-10 22:02:29 +0800

    add func collect

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -26,13 +26,54 @@ const getUserDir = async () => {
 const donwLastVersionTemplate = async (target) => {
   spinner.start('Installing the latest version....waiting ')
   return new Promise((reslove, reject) => {
-    gitClone()
+    gitClone(
+      'https://github.com/Risyen/Mia.bio',
+      target,
+      { shallow: true },
+      (err) => {
+        if (err) return reject(new Error(`About. ${err}`))
+        spinner.succeed(true)
+        spinner.start('Template downloaded. Diff and upgrading...')
+        resolve()
+      }
+    )
   })
 }
 
+const hash = (buf) => createHash('sha1').update(buf).digest('hex')
+
+const ignores = ['node_modules', '.git']
+
+const collect = (currentPath) => {
+  const files = fs.readdirSync(currentPath)
+  const outputs = []
+
+  for (const name of files) {
+    const filePath = path.join(currentPath, name)
+    if (ignores.includes(name)) continue
+    const stat = fs.statSync(filePath)
+
+    if (!stat.isDirectory()) {
+      const data = fs.readFileSync(filePath)
+      outputs.push({
+        name,
+        size: stat.size,
+        hash: hash(data),
+        source: filePath,
+      })
+    } else {
+      // outputs.push(...collect(filePath))
+    }
+  }
+  return outputs
+}
+
+const upgradeFiles = async (tempDir, userDir) => {
+  
+}
+
 ;(async () => {
-  const res = await getTmpBlog()
-  console.log(res)
-  // await getUserDir()
-  await donwLastVersionTemplate()
+  const subPath = path.join(__dirname, '../../')
+  // console.log(subPath)
+  collect(subPath)
 })()
